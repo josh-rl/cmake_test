a28f3b7c4c67e23a8ce134a1b12505f7569f39f0
Joshua Lawrinenko <jrlawrinenko@gmail.com>
2022-12-22 15:50:56 -0600

    learning cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <string>
+#include "math_func.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world! \n";
+    std::string input = "0";
+    std::cout << "Enter number: ";
+    std::cin >> input;
+    math_func* number = new math_func(std::stoi(input));
+
+    int precision = 10;
+    std::cout << "Enter precision: ";
+    std::cin >> input;
+    precision = std::stoi(input);
+
+    std::cout << "Number entered: " << number->get() << std::endl;
+    std::cout << "Square root (precision = 10): " << number->sqrt() << std::endl;
+    std::cout << "Square root (precision = " << precision << "): " << number->sqrt(precision) << std::endl;
+
+    delete number;
     return 0;
 }
 
---
+++ src/math_func.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#ifndef MATH_FUNC_HPP
+#define MATH_FUNC_HPP
+
+class math_func {
+private:
+    int num;
+public:
+    math_func(int num);
+    ~math_func();
+
+    int get();
+    double sqrt();
+    double sqrt(int itr);
+
+    friend std::ostream& operator<<(std::ostream& os, const math_func& self);
+};
+
+math_func::math_func(int num) {
+    this->num = num;
+}
+
+math_func::~math_func() {}
+
+int math_func::get() {
+    return this->num;
+}
+
+double math_func::sqrt() {
+    double ans = this->num;
+    for (int i = 0; i < 10; i++) {
+        ans = 0.5 * (ans + (this->num / ans));
+    }
+    return ans;
+}
+
+double math_func::sqrt(int itr) {
+    double ans = this->num;
+    for (int i = 0; i < itr; i++) {
+        ans = 0.5 * (ans + (this->num / ans));
+    }
+    return ans;
+}
+
+std::ostream& operator<<(std::ostream& os, const math_func& self) {
+    os << self.num;
+    return os;
+}
+
+#endif // MATH_FUNC_HPP
