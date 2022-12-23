42af8de6dea479f57f07c5ee8cd5cc2dd935e0e6
Joshua Lawrinenko <jrlawrinenko@gmail.com>
2022-12-23 12:55:20 -0600

    changes'

---
+++ lib/build/CMakeFiles/engine_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine_lib.dir/math_func.cpp.o"
+  "CMakeFiles/engine_lib.dir/math_func.cpp.o.d"
+  "libengine_lib.a"
+  "libengine_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
